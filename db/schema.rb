3ea61faa6ea7c118c003cf98109f6880ad178ab0
Jacob Kleinen <jaco.kleinen@gmail.com>
2023-06-01 17:29:21 +0800

    jahu

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_023409) do
     t.bigint "user_id"
     t.bigint "listing_id"
     t.datetime "booking_date"
+    t.string "active", default: "true"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["listing_id"], name: "index_bookings_on_listing_id"
