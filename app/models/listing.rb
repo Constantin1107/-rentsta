ec0874462b16f6bda6a25fd195e88a226c964104
eugen oettingen-wallerstein <eugen@oettingen-wallerstein.com>
2023-06-01 10:43:35 +0800

    many photos

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo
-  validates :name, :address, :user_id, :price, :description, :capacity, :photo, presence: true
+  has_many_attached :photos
+  validates :name, :address, :user_id, :price, :description, :capacity, presence: true
 end
