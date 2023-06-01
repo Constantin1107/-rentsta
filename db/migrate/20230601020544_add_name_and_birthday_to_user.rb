8ed04cf96a99b3a3d17448b5d7c04eb3ba6e0f9f
Constantin Leyendecker <constantinleyend@gmail.com>
2023-06-01 10:18:49 +0800

    added my listings and name and birthday to sign up

---
+++ db/migrate/20230601020544_add_name_and_birthday_to_user.rb
@@ -0,0 +1,7 @@
+class AddNameAndBirthdayToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :birthday, :date, null: false
+  end
+end
